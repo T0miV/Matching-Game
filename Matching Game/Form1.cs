4a08bfc86c4338d3079663170438a44e727f3f52
Tomi <dtovi002@edu.xamk.fi>
2022-09-25 18:20:11 +0300

    save 1

--- Matching Game/Form1.cs
+++ Matching Game/Form1.cs
@@ -2,9 +2,44 @@ namespace Matching_Game
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
+
+        List<string> icons = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",", "k", "k",
+            "b", "b", "v", "v", "w", "w", "z", "z"
+
+        };
+
+        Label firstClicked, secondClicked;
         public Form1()
         {
             InitializeComponent();
+            AssignIconsToSquares();
+        }
+
+        private void AssignIconsToSquares()
+        {
+            Label label;
+            int randomNumber;
+
+            for(int i = 0; i< tableLayoutPanel1.Controls.Count; i++)
+            {
+                if (tableLayoutPanel1.Controls[i] is Label)
+                {
+                    label = (Label)tableLayoutPanel1.Controls[i];
+                }
+                else
+                {
+                    continue;
+                }
+
+                randomNumber = random.Next(0, icons.Count);
+                label.Text = icons[randomNumber];
+
+                icons.RemoveAt(randomNumber);
+                   
+            }
         }
     }
 }
