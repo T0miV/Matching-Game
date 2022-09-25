567ebbb684743fb018f4585435d19704b75e5fa1
Tomi <dtovi002@edu.xamk.fi>
2022-09-25 19:13:37 +0300

    all done

--- Matching Game/Form1.cs
+++ Matching Game/Form1.cs
@@ -7,11 +7,11 @@ namespace Matching_Game
         List<string> icons = new List<string>()
         {
             "!", "!", "N", "N", ",", ",", "k", "k",
-            "b", "b", "v", "v", "w", "w", "z", "z"
-
+            "b", "b", "v", "v", "w", "w", "l", "l"
         };
 
         Label firstClicked, secondClicked;
+
         public Form1()
         {
             InitializeComponent();
@@ -23,23 +23,80 @@ namespace Matching_Game
             Label label;
             int randomNumber;
 
-            for(int i = 0; i< tableLayoutPanel1.Controls.Count; i++)
+            for (int i = 0; i < tableLayoutPanel1.Controls.Count; i++)
             {
                 if (tableLayoutPanel1.Controls[i] is Label)
-                {
                     label = (Label)tableLayoutPanel1.Controls[i];
-                }
                 else
-                {
                     continue;
-                }
 
                 randomNumber = random.Next(0, icons.Count);
-                label.Text = icons[randomNumber];
+                string v = icons[randomNumber];
+                label.Text = v;
 
                 icons.RemoveAt(randomNumber);
-                   
             }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+
+            firstClicked = null;
+            secondClicked = null;
+        }
+
+        private void label_Click(object sender, EventArgs e)
+        {
+            if (firstClicked != null && secondClicked != null)
+                return;
+
+            Label clickedLabel = sender as Label;
+
+            if (clickedLabel == null)
+                return;
+
+            if (clickedLabel.ForeColor == Color.Black)
+                return;
+
+            if (firstClicked == null)
+            {
+                firstClicked = clickedLabel;
+                firstClicked.ForeColor = Color.Black;
+                return;
+            }
+
+            secondClicked = clickedLabel;
+            secondClicked.ForeColor = Color.Black;
+
+            CheckForWinner();
+
+            if (firstClicked.Text == secondClicked.Text)
+            {
+                firstClicked = null;
+                secondClicked = null;
+            }
+            else
+                timer1.Start();
+
+        }
+
+        private void CheckForWinner()
+        {
+            Label label;
+            for (int i = 0; i < tableLayoutPanel1.Controls.Count; i++)
+            {
+                label = tableLayoutPanel1.Controls[i] as Label;
+
+                if (label != null && label.ForeColor == label.BackColor)
+                    return;
+            }
+
+            MessageBox.Show("You matches the icons!");
+            Close();
         }
     }
 }
